dab4ed867515e1b5c7f1ca70208a41354363fcd6
Jonathan Nilsson <jonathan@voysys.se>
2019-12-05 00:17:43 +0100

    Better

--- game/src/main.rs
+++ game/src/main.rs
@@ -18,29 +18,48 @@ fn main() {
     let mut x_pos = 0;
     let mut y_pos = 0;
 
+    let mut x_speed = 2;
+    let mut y_speed = 2;
+
     const HALF_WIDTH: i16 = graphics::WIDTH as i16 / 2;
     const HALF_HEIGHT: i16 = graphics::HEIGHT as i16 / 2;
 
     loop {
 
-        controllers.update();
-
-        let mut x_speed = 0;
-        let mut y_speed = 0;
-
-        if controllers.up_pressed() {
-            y_speed = 1;
-        }
+         controllers.update();
 
         x_pos += x_speed;
         y_pos += y_speed;
+
+        if !controllers.up_pressed() {
+
+            if x_pos >= HALF_WIDTH {
+                x_speed = -x_speed;
+                x_pos = HALF_WIDTH;
+            }
+
+            if x_pos <= -HALF_WIDTH {
+                x_speed = -x_speed;
+                x_pos = -HALF_WIDTH;
+            }
+
+            if y_pos >= HALF_HEIGHT {
+                y_speed = -y_speed;
+                y_pos = HALF_HEIGHT;
+            }
+
+            if y_pos <= -HALF_HEIGHT {
+                y_speed = -y_speed;
+                y_pos = -HALF_HEIGHT;
+            }
+        }
 
         graphics::clear_buffer();
 
         ipl3font::draw_str_centered_offset(x_pos, y_pos, WHITE, "Isafo en prutt!");
 
-        graphics::swap_buffers();
-
         graphics::wait_for_vblank();
+
+        graphics::swap_buffers();
     }
 }
