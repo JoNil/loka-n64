ca762b2e08e03cd8ddddf73b1c8130caa4dae00a
Jonathan Nilsson <lord.jonil@gmail.com>
2020-05-03 01:41:05 +0200

    Added aircraft

---
+++ game/maps/tiles/aircraft.tsx
@@ -0,0 +1,265 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="aircraft" tilewidth="81" tileheight="55" tilecount="87" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="57" height="42" source="aircraft/aircraft_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="57" height="42" source="aircraft/aircraft_1_destroyed.png"/>
+ </tile>
+ <tile id="2">
+  <image width="57" height="42" source="aircraft/aircraft_1_hit.png"/>
+ </tile>
+ <tile id="3">
+  <image width="57" height="2" source="aircraft/aircraft_1_prop_1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="57" height="2" source="aircraft/aircraft_1_prop_2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="57" height="2" source="aircraft/aircraft_1_prop_3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="57" height="42" source="aircraft/aircraft_1_shadow.png"/>
+ </tile>
+ <tile id="7">
+  <image width="57" height="42" source="aircraft/aircraft_1b.png"/>
+ </tile>
+ <tile id="8">
+  <image width="57" height="42" source="aircraft/aircraft_1b_destroyed.png"/>
+ </tile>
+ <tile id="9">
+  <image width="57" height="42" source="aircraft/aircraft_1b_hit.png"/>
+ </tile>
+ <tile id="10">
+  <image width="57" height="2" source="aircraft/aircraft_1b_prop_1.png"/>
+ </tile>
+ <tile id="11">
+  <image width="57" height="2" source="aircraft/aircraft_1b_prop_2.png"/>
+ </tile>
+ <tile id="12">
+  <image width="57" height="2" source="aircraft/aircraft_1b_prop_3.png"/>
+ </tile>
+ <tile id="13">
+  <image width="57" height="42" source="aircraft/aircraft_1c.png"/>
+ </tile>
+ <tile id="14">
+  <image width="57" height="42" source="aircraft/aircraft_1c_destroyed.png"/>
+ </tile>
+ <tile id="15">
+  <image width="57" height="42" source="aircraft/aircraft_1c_hit.png"/>
+ </tile>
+ <tile id="16">
+  <image width="57" height="2" source="aircraft/aircraft_1c_prop_1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="57" height="2" source="aircraft/aircraft_1c_prop_2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="57" height="2" source="aircraft/aircraft_1c_prop_3.png"/>
+ </tile>
+ <tile id="19">
+  <image width="57" height="42" source="aircraft/aircraft_1d.png"/>
+ </tile>
+ <tile id="20">
+  <image width="57" height="42" source="aircraft/aircraft_1d_destroyed.png"/>
+ </tile>
+ <tile id="21">
+  <image width="57" height="42" source="aircraft/aircraft_1d_hit.png"/>
+ </tile>
+ <tile id="22">
+  <image width="57" height="2" source="aircraft/aircraft_1d_prop_1.png"/>
+ </tile>
+ <tile id="23">
+  <image width="57" height="2" source="aircraft/aircraft_1d_prop_2.png"/>
+ </tile>
+ <tile id="24">
+  <image width="57" height="2" source="aircraft/aircraft_1d_prop_3.png"/>
+ </tile>
+ <tile id="25">
+  <image width="57" height="42" source="aircraft/aircraft_1e.png"/>
+ </tile>
+ <tile id="26">
+  <image width="57" height="42" source="aircraft/aircraft_1e_destroyed.png"/>
+ </tile>
+ <tile id="27">
+  <image width="57" height="42" source="aircraft/aircraft_1e_hit.png"/>
+ </tile>
+ <tile id="28">
+  <image width="57" height="2" source="aircraft/aircraft_1e_prop_1.png"/>
+ </tile>
+ <tile id="29">
+  <image width="57" height="2" source="aircraft/aircraft_1e_prop_2.png"/>
+ </tile>
+ <tile id="30">
+  <image width="57" height="2" source="aircraft/aircraft_1e_prop_3.png"/>
+ </tile>
+ <tile id="31">
+  <image width="63" height="47" source="aircraft/aircraft_2.png"/>
+ </tile>
+ <tile id="32">
+  <image width="63" height="47" source="aircraft/aircraft_2_destroyed.png"/>
+ </tile>
+ <tile id="33">
+  <image width="63" height="47" source="aircraft/aircraft_2_hit.png"/>
+ </tile>
+ <tile id="34">
+  <image width="63" height="2" source="aircraft/aircraft_2_prop_1.png"/>
+ </tile>
+ <tile id="35">
+  <image width="63" height="2" source="aircraft/aircraft_2_prop_2.png"/>
+ </tile>
+ <tile id="36">
+  <image width="63" height="2" source="aircraft/aircraft_2_prop_3.png"/>
+ </tile>
+ <tile id="37">
+  <image width="63" height="47" source="aircraft/aircraft_2_shadow.png"/>
+ </tile>
+ <tile id="38">
+  <image width="63" height="47" source="aircraft/aircraft_2b.png"/>
+ </tile>
+ <tile id="39">
+  <image width="63" height="47" source="aircraft/aircraft_2b_destroyed.png"/>
+ </tile>
+ <tile id="40">
+  <image width="63" height="47" source="aircraft/aircraft_2b_hit.png"/>
+ </tile>
+ <tile id="41">
+  <image width="63" height="2" source="aircraft/aircraft_2b_prop_1.png"/>
+ </tile>
+ <tile id="42">
+  <image width="63" height="2" source="aircraft/aircraft_2b_prop_2.png"/>
+ </tile>
+ <tile id="43">
+  <image width="63" height="2" source="aircraft/aircraft_2b_prop_3.png"/>
+ </tile>
+ <tile id="44">
+  <image width="63" height="47" source="aircraft/aircraft_2c.png"/>
+ </tile>
+ <tile id="45">
+  <image width="63" height="47" source="aircraft/aircraft_2c_destroyed.png"/>
+ </tile>
+ <tile id="46">
+  <image width="63" height="47" source="aircraft/aircraft_2c_hit.png"/>
+ </tile>
+ <tile id="47">
+  <image width="63" height="2" source="aircraft/aircraft_2c_prop_1.png"/>
+ </tile>
+ <tile id="48">
+  <image width="63" height="2" source="aircraft/aircraft_2c_prop_2.png"/>
+ </tile>
+ <tile id="49">
+  <image width="63" height="2" source="aircraft/aircraft_2c_prop_3.png"/>
+ </tile>
+ <tile id="50">
+  <image width="63" height="47" source="aircraft/aircraft_2d.png"/>
+ </tile>
+ <tile id="51">
+  <image width="63" height="47" source="aircraft/aircraft_2d_destroyed.png"/>
+ </tile>
+ <tile id="52">
+  <image width="63" height="47" source="aircraft/aircraft_2d_hit.png"/>
+ </tile>
+ <tile id="53">
+  <image width="63" height="2" source="aircraft/aircraft_2d_prop_1.png"/>
+ </tile>
+ <tile id="54">
+  <image width="63" height="2" source="aircraft/aircraft_2d_prop_2.png"/>
+ </tile>
+ <tile id="55">
+  <image width="63" height="2" source="aircraft/aircraft_2d_prop_3.png"/>
+ </tile>
+ <tile id="56">
+  <image width="63" height="47" source="aircraft/aircraft_2e.png"/>
+ </tile>
+ <tile id="57">
+  <image width="63" height="47" source="aircraft/aircraft_2e_destroyed.png"/>
+ </tile>
+ <tile id="58">
+  <image width="63" height="47" source="aircraft/aircraft_2e_hit.png"/>
+ </tile>
+ <tile id="59">
+  <image width="63" height="2" source="aircraft/aircraft_2e_prop_1.png"/>
+ </tile>
+ <tile id="60">
+  <image width="63" height="2" source="aircraft/aircraft_2e_prop_2.png"/>
+ </tile>
+ <tile id="61">
+  <image width="63" height="2" source="aircraft/aircraft_2e_prop_3.png"/>
+ </tile>
+ <tile id="62">
+  <image width="81" height="55" source="aircraft/aircraft_3.png"/>
+ </tile>
+ <tile id="63">
+  <image width="81" height="55" source="aircraft/aircraft_3_destroyed.png"/>
+ </tile>
+ <tile id="64">
+  <image width="81" height="55" source="aircraft/aircraft_3_hit.png"/>
+ </tile>
+ <tile id="65">
+  <image width="81" height="2" source="aircraft/aircraft_3_prop_1.png"/>
+ </tile>
+ <tile id="66">
+  <image width="81" height="2" source="aircraft/aircraft_3_prop_2.png"/>
+ </tile>
+ <tile id="67">
+  <image width="81" height="2" source="aircraft/aircraft_3_prop_3.png"/>
+ </tile>
+ <tile id="68">
+  <image width="81" height="55" source="aircraft/aircraft_3_shadow.png"/>
+ </tile>
+ <tile id="69">
+  <image width="81" height="55" source="aircraft/aircraft_3b.png"/>
+ </tile>
+ <tile id="70">
+  <image width="81" height="55" source="aircraft/aircraft_3b_destroyed.png"/>
+ </tile>
+ <tile id="71">
+  <image width="81" height="55" source="aircraft/aircraft_3b_hit.png"/>
+ </tile>
+ <tile id="72">
+  <image width="81" height="2" source="aircraft/aircraft_3b_prop_1.png"/>
+ </tile>
+ <tile id="73">
+  <image width="81" height="2" source="aircraft/aircraft_3b_prop_2.png"/>
+ </tile>
+ <tile id="74">
+  <image width="81" height="2" source="aircraft/aircraft_3b_prop_3.png"/>
+ </tile>
+ <tile id="75">
+  <image width="81" height="55" source="aircraft/aircraft_3d.png"/>
+ </tile>
+ <tile id="76">
+  <image width="81" height="55" source="aircraft/aircraft_3d_destroyed.png"/>
+ </tile>
+ <tile id="77">
+  <image width="81" height="55" source="aircraft/aircraft_3d_hit.png"/>
+ </tile>
+ <tile id="78">
+  <image width="81" height="2" source="aircraft/aircraft_3d_prop_1.png"/>
+ </tile>
+ <tile id="79">
+  <image width="81" height="2" source="aircraft/aircraft_3d_prop_2.png"/>
+ </tile>
+ <tile id="80">
+  <image width="81" height="2" source="aircraft/aircraft_3d_prop_3.png"/>
+ </tile>
+ <tile id="81">
+  <image width="81" height="55" source="aircraft/aircraft_3e.png"/>
+ </tile>
+ <tile id="82">
+  <image width="81" height="55" source="aircraft/aircraft_3e_destroyed.png"/>
+ </tile>
+ <tile id="83">
+  <image width="81" height="55" source="aircraft/aircraft_3e_hit.png"/>
+ </tile>
+ <tile id="84">
+  <image width="81" height="2" source="aircraft/aircraft_3e_prop_1.png"/>
+ </tile>
+ <tile id="85">
+  <image width="81" height="2" source="aircraft/aircraft_3e_prop_2.png"/>
+ </tile>
+ <tile id="86">
+  <image width="81" height="2" source="aircraft/aircraft_3e_prop_3.png"/>
+ </tile>
+</tileset>
