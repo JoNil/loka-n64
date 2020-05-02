3809723d6e861d2d0802519af884062a082cc782
Jonathan Nilsson <jonathan@voysys.se>
2020-05-03 00:47:01 +0200

    Added map tiles and stuff

---
+++ game/maps/tiles/space-blks-1.034.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="space-blks-1.034" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="space-blks-1.034.png" width="1024" height="768"/>
+</tileset>
