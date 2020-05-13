6bc345ed483c022c0b7274123c0a2183e816ff4d
Jonathan Nilsson <jonathan@voysys.se>
2020-05-14 00:47:43 +0200

    Map drawing

--- game/src/map.rs
+++ game/src/map.rs
@@ -22,12 +22,10 @@ impl Map {
     pub fn render(&self, cb: &mut CommandBuffer, video_mode: VideoMode, camera: &Camera) {
         let tiles_in_layer = (self.data.width_in_tiles * self.data.height_in_tiles) as usize;
 
-        let tile_scale: Vec2 = Vec2::new(
-            32.0 / video_mode.width() as f32,
-            32.0 / video_mode.height() as f32,
-        );
+        let tile_scale: Vec2 = Vec2::new(32.0, 32.0);
 
-        let camera_tile = camera.pos / Vec2::new(self.data.tile_width as f32, self.data.tile_height as f32);
+        let camera_tile =
+            camera.pos / Vec2::new(self.data.tile_width as f32, self.data.tile_height as f32);
 
         let tiles_on_screen_x = (video_mode.width() / self.data.tile_width) + 2;
         let tiles_on_screen_y = (video_mode.height() / self.data.tile_height) + 2;
@@ -36,15 +34,12 @@ impl Map {
         let first_tile_y = camera_tile.y().floor() as i32;
 
         for layer in self.data.layers.chunks_exact(tiles_in_layer) {
-
             for y in first_tile_y..(first_tile_y + tiles_on_screen_y) {
-
                 if y < 0 || y >= self.data.height_in_tiles {
                     continue;
                 }
 
                 for x in first_tile_x..(first_tile_x + tiles_on_screen_x) {
-
                     if x < 0 || x >= self.data.width_in_tiles {
                         continue;
                     }
@@ -56,18 +51,19 @@ impl Map {
                         continue;
                     }
 
-                    let pos = Vec2::new(x as f32, y as f32) * tile_scale;
+                    let pos = Vec2::new(
+                        (x * self.data.tile_width) as f32,
+                        (y * self.data.tile_height) as f32,
+                    );
 
                     let half_size = tile_scale / 2.0;
 
                     let upper_left = pos - half_size;
                     let lower_right = pos + half_size;
 
-                    let screen_size = Vec2::new(video_mode.width() as f32, video_mode.height() as f32);
-
                     cb.add_textured_rect(
-                        upper_left * screen_size - camera.pos,
-                        lower_right * screen_size - camera.pos,
+                        upper_left - camera.pos,
+                        lower_right - camera.pos,
                         MAP_1_TILES[(tile - 1) as usize].as_texture(),
                     );
                 }
