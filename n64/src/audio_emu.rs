ffec92cae8e72b4f0e3f39b2206872883a158f99
Jonathan Nilsson <jonathan@voysys.se>
2020-05-17 23:33:34 +0200

    Worked first try

--- n64/src/audio_emu.rs
+++ n64/src/audio_emu.rs
@@ -1,40 +1,46 @@
 use cpal::{
     self,
     traits::{DeviceTrait, EventLoopTrait, HostTrait},
-    StreamData, UnknownTypeOutputBuffer, Format,
+    StreamData, UnknownTypeOutputBuffer,
 };
 use std::error::Error;
 use std::{
     sync::mpsc::{channel, Receiver, Sender},
     thread,
 };
-use sample::{interpolate, ring_buffer, signal, Sample, Signal};
+use rubato::{Resampler, SincFixedIn, InterpolationType, InterpolationParameters, WindowFunction};
 
 pub const BUFFER_NO_SAMPLES: usize = 2 * 512;
 pub const BUFFER_COUNT: usize = 4;
 
 fn get_sample<T>(
-    format: &Format,
-    //sinc: &mut interpolate::Sinc<[[f32; 1]; 100]>,
+    resampler: &mut SincFixedIn<f32>,
     current_index: &mut usize,
-    current_buffer: &mut Option<Vec<i16>>,
+    current_buffer: &mut Option<Vec<f32>>,
     to_audio_receiver: &Receiver<Buffer>,
     from_audio_sender: &Sender<Buffer>,
-    sample_convert: impl Fn(i16) -> T,
+    sample_convert: impl Fn(f32) -> T,
 ) -> T {
     if current_buffer.is_none() {
         if let Ok(buffer) = to_audio_receiver.try_recv() {
 
-            let samples = buffer.samples.iter().map(|s| i16::to_sample::<f32>(*s));
-            let signal = signal::from_interleaved_samples_iter(samples);
+            let mut channels = Vec::new();    
+            channels.push(Vec::with_capacity(BUFFER_NO_SAMPLES / 2));
+            channels.push(Vec::with_capacity(BUFFER_NO_SAMPLES / 2));
 
+            for frame in buffer.samples.chunks_exact(2) {
+                channels[0].push(frame[0] as f32 / (i16::MAX as f32));
+                channels[1].push(frame[1] as f32 / (i16::MAX as f32));
+            }
 
-            let ring_buffer = ring_buffer::Fixed::from([[0.0]; 100]);
-            let sinc = interpolate::Sinc::new(ring_buffer);
-            
-            let new_signal = signal.from_hz_to_hz(sinc, 22050 as f64, format.sample_rate.0 as f64);
+            let resampled_buffers = resampler.process(&channels).unwrap();
 
-            let converted_buffer = new_signal.until_exhausted().map().collect::<Vec<_>>();
+            let mut converted_buffer = Vec::with_capacity(resampled_buffers[0].len() + resampled_buffers[1].len());
+
+            for samples in resampled_buffers[0].iter().zip(resampled_buffers[1].iter()) {
+                converted_buffer.push(*samples.0);
+                converted_buffer.push(*samples.1);
+            }
 
             *current_buffer = Some(converted_buffer);
             *current_index = 0;
@@ -43,21 +49,19 @@ fn get_sample<T>(
         }
     }
 
-    let sample = if let Some(buffer) = current_buffer {
+    sample_convert(if let Some(buffer) = current_buffer {
         let sample = buffer[*current_index];
 
         *current_index += 1;
 
         if *current_index >= buffer.len() {
-            current_buffer = None;
+            *current_buffer = None;
         }
 
-        sample_convert(sample)
+        sample
     } else {
-        sample_convert(0)
-    };
-
-    sample
+        0.0
+    })
 }
 
 fn audio_thread(
@@ -76,7 +80,20 @@ fn audio_thread(
 
     event_loop.play_stream(stream_id)?;
 
-    //let sinc = interpolate::Sinc::new(ring_buffer::Fixed::from([[0.0]; 100]));
+    let params = InterpolationParameters {
+        sinc_len: 64,
+        f_cutoff: 0.95,
+        interpolation: InterpolationType::Nearest,
+        oversampling_factor: 160,
+        window: WindowFunction::BlackmanHarris2,
+    };
+
+    let mut resampler = SincFixedIn::<f32>::new(
+        format.sample_rate.0 as f64 / 22050 as f64,
+        params,
+        BUFFER_NO_SAMPLES / 2,
+        2,
+    );
 
     let mut current_buffer = None;
     let mut current_index = 0;
@@ -96,13 +113,12 @@ fn audio_thread(
             } => {
                 for elem in buffer.iter_mut() {
                     *elem = get_sample(
-                        &format,
-                        //&mut sinc,
+                        &mut resampler,
                         &mut current_index,
                         &mut current_buffer,
                         &to_audio_receiver,
                         &from_audio_sender,
-                        |sample| (sample as f32 + (u16::MAX / 2) as f32) as u16,
+                        |sample| (sample + (u16::MAX / 2) as f32) as u16,
                     );
                 }
             }
@@ -111,13 +127,12 @@ fn audio_thread(
             } => {
                 for elem in buffer.iter_mut() {
                     *elem = get_sample(
-                        &format,
-                        //&mut sinc,
+                        &mut resampler,
                         &mut current_index,
                         &mut current_buffer,
                         &to_audio_receiver,
                         &from_audio_sender,
-                        |sample| sample,
+                        |sample| (sample / (i16::MAX as f32)) as i16,
                     );
                 }
             }
@@ -126,13 +141,12 @@ fn audio_thread(
             } => {
                 for elem in buffer.iter_mut() {
                     *elem = get_sample(
-                        &format,
-                        //&mut sinc,
+                        &mut resampler,
                         &mut current_index,
                         &mut current_buffer,
                         &to_audio_receiver,
                         &from_audio_sender,
-                        |sample| sample as f32 / (i16::MAX as f32),
+                        |sample| sample as f32,
                     );
                 }
             }
