d4fe09418445662e2b861e9d317af106e01b2d31
Jonathan Nilsson <jonathan@voysys.se>
2021-11-09 23:25:41 +0100

    Print panic message

--- game/src/main.rs
+++ game/src/main.rs
@@ -267,6 +267,8 @@ fn start(_argc: isize, _argv: *const *const u8) -> isize {
 #[cfg(target_vendor = "nintendo64")]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
+    n64::debugln!("{}", &info);
+
     const GREEN: Color = Color::new(0b00011_10000_00011_1);
 
     let mut out_tex = n64::gfx::TextureMut::new(VIDEO_MODE.width(), VIDEO_MODE.height(), unsafe {
