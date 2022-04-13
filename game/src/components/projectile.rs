7e663b6fd7d26dcf73065a881d74dc8126558213
Jonathan Nilsson <jonathan@voysys.se>
2022-04-14 01:27:13 +0200

    Only check projectile vs projectile once

--- game/src/components/projectile.rs
+++ game/src/components/projectile.rs
@@ -21,8 +21,14 @@ pub fn update(world: &mut World, camera: &Camera) {
 
     let camera_bb: Aabb2 = Aabb2::new(camera.pos, camera.pos + vec2(1.0, 1.0));
 
-    for (p, entity) in projectile.components_and_entities() {
-        if let (Some(m), Some(s)) = (movable.lookup(entity), size.lookup(entity)) {
+    let projectiles = projectile.components();
+    let entities = projectile.entities();
+
+    for i in 0..projectiles.len() {
+        let p1 = &projectiles[i];
+        let e1 = entities[i];
+
+        if let (Some(m), Some(s)) = (movable.lookup(e1), size.lookup(e1)) {
             let mut delete = false;
             let projectile_bb = Aabb2::from_center_size(m.pos, s.size);
 
@@ -30,7 +36,7 @@ pub fn update(world: &mut World, camera: &Camera) {
                 delete = true;
             }
 
-            if p.target_type == WeaponTarget::Enemy {
+            if p1.target_type == WeaponTarget::Enemy {
                 for enemy_entity in enemy.entities() {
                     if let (Some(m2), Some(s2)) =
                         (movable.lookup(*enemy_entity), size.lookup(*enemy_entity))
@@ -38,14 +44,14 @@ pub fn update(world: &mut World, camera: &Camera) {
                         let enemy_bb = Aabb2::from_center_size(m2.pos, s2.size);
 
                         if projectile_bb.collides(&enemy_bb) {
-                            health::damage(health, *enemy_entity, p.damage);
+                            health::damage(health, *enemy_entity, p1.damage);
                             delete = true;
                         }
                     }
                 }
             }
 
-            if p.target_type == WeaponTarget::Player {
+            if p1.target_type == WeaponTarget::Player {
                 for player_entity in player.entities() {
                     if let (Some(m2), Some(s2)) =
                         (movable.lookup(*player_entity), size.lookup(*player_entity))
@@ -53,32 +59,33 @@ pub fn update(world: &mut World, camera: &Camera) {
                         let player_bb = Aabb2::from_center_size(m2.pos, s2.size);
 
                         if projectile_bb.collides(&player_bb) {
-                            health::damage(health, *player_entity, p.damage);
+                            health::damage(health, *player_entity, p1.damage);
                             delete = true;
                         }
                     }
                 }
             }
 
-            for (p2, entity2) in projectile.components_and_entities() {
-                if entity != entity2 {
-                    if let (Some(m2), Some(s2)) = (movable.lookup(entity2), size.lookup(entity2)) {
-                        let projectile_bb_2 = Aabb2::from_center_size(m2.pos, s2.size);
+            for j in (i + 1)..projectiles.len() {
+                let p2 = &projectiles[j];
+                let e2 = entities[j];
 
-                        if projectile_bb.collides(&projectile_bb_2) {
-                            health::damage(health, entity2, p.damage);
-                            health::damage(health, entity, p2.damage);
-                        }
+                if let (Some(m2), Some(s2)) = (movable.lookup(e2), size.lookup(e2)) {
+                    let projectile_bb_2 = Aabb2::from_center_size(m2.pos, s2.size);
+
+                    if projectile_bb.collides(&projectile_bb_2) {
+                        health::damage(health, e2, p1.damage);
+                        health::damage(health, e1, p2.damage);
                     }
                 }
             }
 
-            if !health::is_alive(health, entity) {
+            if !health::is_alive(health, e1) {
                 delete = true;
             }
 
             if delete {
-                world.entities.despawn(entity);
+                world.entities.despawn(e1);
             }
         }
     }
