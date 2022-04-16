ceee86ea41c2793abbe3ceb29d51206ffaa9ff9b
Jonathan Nilsson <jonathan@voysys.se>
2022-04-16 03:15:52 +0200

    Fix

--- n64/src/graphics_emu/textured_rect.rs
+++ n64/src/graphics_emu/textured_rect.rs
@@ -163,7 +163,7 @@ impl TexturedRect {
             min_filter: wgpu::FilterMode::Linear,
             mipmap_filter: wgpu::FilterMode::Linear,
             lod_min_clamp: 0.0,
-            lod_max_clamp: 100.0,
+            lod_max_clamp: f32::MAX,
             compare: None,
             anisotropy_clamp: None,
             border_color: None,
