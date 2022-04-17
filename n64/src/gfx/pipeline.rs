71b8e6db3b702c37b4da83c86109b43b32d9dcc3
Jonathan Nilsson <jonathan@voysys.se>
2022-04-18 00:40:40 +0200

    More stuff in pipelein

--- n64/src/gfx/pipeline.rs
+++ n64/src/gfx/pipeline.rs
@@ -1,11 +1,30 @@
 use super::{color_combiner::ColorCombiner, Texture};
 
-#[derive(Default)]
+pub enum CycleType {
+    One,
+    Two,
+}
+
 pub struct Pipeline {
+    pub cycle_type: CycleType,
+
     pub combiner_mode: ColorCombiner,
     pub prim_color: Option<u32>,
     pub env_color: Option<u32>,
 
     pub blend_color: Option<u32>,
     pub texture: Option<Texture<'static>>,
+}
+
+impl Default for Pipeline {
+    fn default() -> Self {
+        Self {
+            cycle_type: CycleType::One,
+            combiner_mode: Default::default(),
+            prim_color: Default::default(),
+            env_color: Default::default(),
+            blend_color: Default::default(),
+            texture: Default::default(),
+        }
+    }
 }
