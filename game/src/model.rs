10defb8c1afc568c1f77d142320235e1ccf08557
Jonathan Nilsson <jonathan@voysys.se>
2022-05-01 11:34:03 +0200

    No cfg if

--- game/src/model.rs
+++ game/src/model.rs
@@ -21,78 +21,78 @@ pub struct StaticModelData {
 
 impl StaticModelData {
     pub fn as_model_data(&self) -> ModelData {
-        cfg_if::cfg_if! {
-            if #[cfg(target_vendor = "nintendo64")] {
+        #[cfg(target_vendor = "nintendo64")]
+        {
+            let verts = LayoutVerified::<_, [[f32; 3]]>::new_slice(self.verts)
+                .unwrap()
+                .into_slice();
 
-                let verts = LayoutVerified::<_, [[f32; 3]]>::new_slice(self.verts)
-                    .unwrap()
-                    .into_slice();
+            let uvs = LayoutVerified::<_, [[f32; 2]]>::new_slice(self.uvs)
+                .unwrap()
+                .into_slice();
 
-                let uvs = LayoutVerified::<_, [[f32; 2]]>::new_slice(self.uvs)
-                    .unwrap()
-                    .into_slice();
+            let colors = LayoutVerified::<_, [u32]>::new_slice(self.colors)
+                .unwrap()
+                .into_slice();
 
-                let colors = LayoutVerified::<_, [u32]>::new_slice(self.colors)
-                    .unwrap()
-                    .into_slice();
+            let indices = LayoutVerified::<_, [[u8; 3]]>::new_slice(self.indices)
+                .unwrap()
+                .into_slice();
 
-                let indices = LayoutVerified::<_, [[u8; 3]]>::new_slice(self.indices)
-                    .unwrap()
-                    .into_slice();
+            ModelData {
+                verts: Cow::Borrowed(verts),
+                uvs: Cow::Borrowed(uvs),
+                colors: Cow::Borrowed(colors),
+                indices: Cow::Borrowed(indices),
+                size: self.size,
+            }
+        }
 
-                ModelData {
-                    verts: Cow::Borrowed(verts),
-                    uvs: Cow::Borrowed(uvs),
-                    colors: Cow::Borrowed(colors),
-                    indices: Cow::Borrowed(indices),
-                    size: self.size,
-                }
-            } else {
+        #[cfg(not(target_vendor = "nintendo64"))]
+        {
+            fn byteswap_u32_slice(data: &[u8]) -> Vec<u8> {
+                let mut res = Vec::with_capacity(data.len());
 
-                fn byteswap_u32_slice(data: &[u8]) -> Vec<u8> {
-                    let mut res = Vec::with_capacity(data.len());
-
-                    for part in data.chunks_exact(4) {
-                        res.push(part[3]);
-                        res.push(part[2]);
-                        res.push(part[1]);
-                        res.push(part[0]);
-                    }
-
-                    res
+                for part in data.chunks_exact(4) {
+                    res.push(part[3]);
+                    res.push(part[2]);
+                    res.push(part[1]);
+                    res.push(part[0]);
                 }
 
-                let verts_in = byteswap_u32_slice(self.verts);
-                let uvs_in = byteswap_u32_slice(self.uvs);
-                let colors_in = byteswap_u32_slice(self.colors);
-                let indices_in = self.indices;
+                res
+            }
 
-                let verts = LayoutVerified::<_, [[f32; 3]]>::new_slice(verts_in.as_slice())
-                    .unwrap()
-                    .into_slice()
-                    .to_owned();
+            let verts_in = byteswap_u32_slice(self.verts);
+            let uvs_in = byteswap_u32_slice(self.uvs);
+            let colors_in = byteswap_u32_slice(self.colors);
+            let indices_in = self.indices;
 
-                let uvs = LayoutVerified::<_, [[f32; 2]]>::new_slice(uvs_in.as_slice())
-                    .unwrap()
-                    .into_slice()
-                    .to_owned();
+            let verts = LayoutVerified::<_, [[f32; 3]]>::new_slice(verts_in.as_slice())
+                .unwrap()
+                .into_slice()
+                .to_owned();
 
-                let colors = LayoutVerified::<_, [u32]>::new_slice(colors_in.as_slice())
-                    .unwrap()
-                    .into_slice()
-                    .to_owned();
+            let uvs = LayoutVerified::<_, [[f32; 2]]>::new_slice(uvs_in.as_slice())
+                .unwrap()
+                .into_slice()
+                .to_owned();
 
-                let indices = LayoutVerified::<_, [[u8; 3]]>::new_slice(indices_in)
-                    .unwrap()
-                    .into_slice();
+            let colors = LayoutVerified::<_, [u32]>::new_slice(colors_in.as_slice())
+                .unwrap()
+                .into_slice()
+                .to_owned();
 
-                ModelData {
-                    verts: Cow::Owned(verts),
-                    uvs: Cow::Owned(uvs),
-                    colors: Cow::Owned(colors),
-                    indices: Cow::Borrowed(indices),
-                    size: self.size,
-                }
+            let indices = LayoutVerified::<_, [[u8; 3]]>::new_slice(indices_in)
+                .unwrap()
+                .into_slice();
+
+            ModelData {
+                verts: Cow::Owned(verts),
+                uvs: Cow::Owned(uvs),
+                colors: Cow::Owned(colors),
+                indices: Cow::Borrowed(indices),
+                size: self.size,
             }
         }
     }
