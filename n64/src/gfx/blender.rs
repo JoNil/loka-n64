7308a10558b739eafba075918ac475cc9cce85e1
Jonathan Nilsson <jonathan@voysys.se>
2022-05-07 15:44:28 +0200

    Blend mode command

--- n64/src/gfx/blender.rs
+++ n64/src/gfx/blender.rs
@@ -1,8 +1,11 @@
 #![allow(dead_code)]
 
+use strum_macros::FromRepr;
+
 // color = (a*p + b*m) / (a + b)
 
-#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, FromRepr)]
 enum PMCycleOne {
     ColorCombinerRgb = 0,
     Memory = 1,
@@ -10,7 +13,8 @@ enum PMCycleOne {
     FogColor = 3,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, FromRepr)]
 enum PMCycleTwo {
     FirstCycleNumerator = 0,
     Memory = 1,
@@ -18,7 +22,8 @@ enum PMCycleTwo {
     FogColor = 3,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, FromRepr)]
 enum ASrc {
     ColorCombinerAlpha = 0,
     FogAlpha = 1,
@@ -26,7 +31,8 @@ enum ASrc {
     Zero = 3,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, FromRepr)]
 enum BSrc {
     OneMinusA = 0,
     MemoryAlpha = 1,
@@ -34,7 +40,8 @@ enum BSrc {
     Zero = 3,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, FromRepr)]
 enum RgbDither {
     MagicSquareMatrix = 0,
     StandardBayerMatrix = 1,
@@ -42,7 +49,8 @@ enum RgbDither {
     NoDither = 3,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, FromRepr)]
 enum AlphaDither {
     Pattern = 0,
     PatternInverted = 1,
@@ -87,7 +95,17 @@ impl Blender {
 
 impl Blender {
     pub fn to_command(&self) -> u64 {
-        0
+        let a_0 = (self.a_0 as u64) << 30;
+        let p_0 = (self.p_0 as u64) << 26;
+        let b_0 = (self.b_0 as u64) << 22;
+        let m_0 = (self.m_0 as u64) << 18;
+
+        let a_1 = (self.a_1 as u64) << 28;
+        let p_1 = (self.p_1 as u64) << 24;
+        let b_1 = (self.b_1 as u64) << 20;
+        let m_1 = (self.m_1 as u64) << 16;
+
+        a_0 | p_0 | b_0 | m_0 | a_1 | p_1 | b_1 | m_1
     }
 }
 
