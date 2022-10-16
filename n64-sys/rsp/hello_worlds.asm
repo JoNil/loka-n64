200257242a4b4337c5641ca71d12e945ea66fa9a
Jonathan Nilsson <jonathan@voysys.se>
2022-10-16 22:14:15 +0200

    Less comments

--- n64-sys/rsp/hello_worlds.asm
+++ n64-sys/rsp/hello_worlds.asm
@@ -2,8 +2,8 @@ arch n64.rsp
 endian msb
 output "hello_world.bin", create
 
-include "lib/n64.inc" // Include N64 Definitions
-include "lib/n64_rsp.inc" // Include 64 Byte Header & Vector Table
+include "lib/n64.inc"
+include "lib/n64_rsp.inc"
 
 base $0000
 origin $0000
