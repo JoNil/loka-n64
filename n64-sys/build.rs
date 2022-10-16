83e8f2ca172e0b658d270a5ee74baa07292cc09f
Jonathan Nilsson <jonathan@voysys.se>
2022-10-16 23:23:01 +0200

    wierd

--- n64-sys/build.rs
+++ n64-sys/build.rs
@@ -1,4 +1,24 @@
+use std::{env, fs, path::Path, process::Command};
+
 fn main() {
-    // bass RSPPlotTriangle.asm
-    panic!("Weee");
+    println!("cargo:rerun-if-changed=rsp");
+
+    let bass = Path::new("../tools/bass/bass.exe").canonicalize().unwrap();
+    let bass = bass.to_string_lossy();
+    let bass = bass.trim_start_matches("\\\\?\\");
+
+    for file in fs::read_dir("rsp").unwrap().filter_map(|e| e.ok()) {
+        let path = file.path();
+        let ext = path.extension().map(|s| s.to_string_lossy());
+
+        if let Some(ext) = ext {
+            if ext == "asm" {
+                let path = path.canonicalize().unwrap();
+                let path = path.to_string_lossy();
+                let path = path.trim_start_matches("\\\\?\\");
+
+                assert!(Command::new(&bass).arg(path).status().is_ok());
+            }
+        }
+    }
 }
