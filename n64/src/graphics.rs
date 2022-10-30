a102d010b6eba738b2f73a70378a7fd7640885f8
Jonathan Nilsson <jonathan@voysys.se>
2022-10-30 15:07:07 +0100

    Test assembly

--- n64/src/graphics.rs
+++ n64/src/graphics.rs
@@ -1,4 +1,6 @@
+use crate::include_bytes_align_as;
 use crate::{current_time_us, framebuffer::Framebuffer, VideoMode};
+use n64_macros::debugln;
 use n64_sys::{rdp, rsp, vi};
 
 pub struct Graphics {}
@@ -28,8 +30,22 @@ impl Graphics {
 
     #[inline]
     pub fn rsp_hello_world(&self) {
-        let code = include_bytes!("../../n64-sys/rsp/hello_world.bin");
+        let code = include_bytes_align_as!(u64, "../../n64-sys/rsp/hello_world.bin");
 
         rsp::run(code, None);
+
+        let mut dmem = [0u8; 4096];
+
+        rsp::read_dmem(&mut dmem);
+
+        for (i, word) in dmem.windows(4).enumerate() {
+            let a = u32::from_be_bytes(word.try_into().unwrap());
+
+            debugln!("{} == {}", a, i);
+
+            assert!(a == i as u32);
+        }
+
+        panic!("DONE");
     }
 }
