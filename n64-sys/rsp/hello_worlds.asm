a748347f8387f67173d9aab3e05047692ab5d236
Victor Johansson <victor.johansson.42@gmail.com>
2022-10-30 14:48:05 +0100

    Initial assembler test

--- n64-sys/rsp/hello_worlds.asm
+++ n64-sys/rsp/hello_worlds.asm
@@ -10,5 +10,36 @@ origin $0000
 
 align(8)
 start:
-    j start
+    xor t0, t0, t0
+write_addr:
+    sw  t1, 0(t1)
+    xor t1, t1, t1 // Zero offset
+    bne t0, 4096, write_addr
+    j return
+
+// Zero t0
+xor t0, t0, t0
+
+handle_commands:
+    // t0 contains ptr
+    j poll
+
+poll:
+    la t0, 0
+    beq t0, 0, poll
+    nop
+    j handle_commands
+
+start_2:
+    xor t1, t1, t1 // Zero offset
+    j loop
+handle:
+
+loop:    
+    la t0, 0   // Load operation
+    bne t0, 0, handle
+    j poll
     nop // Delay Slot
+
+return:
+    break
