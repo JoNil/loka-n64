47982836dc3e53dade1d4ff7c26256a1a589d5e0
Victor Johansson <victor.johansson.42@gmail.com>
2023-01-22 19:58:42 +0100

    Fixed crash if vu register index was > 31 in editor.

--- vu-emu/src/instruction.rs
+++ vu-emu/src/instruction.rs
@@ -4,22 +4,22 @@ use crate::{Reg, Vu};
 
 #[derive(Copy, Clone, FromStr)]
 pub enum Instruction {
-    #[display("vmudl {0},{1},{2}")]
-    Vmudl(Reg, Reg, Reg),
-    #[display("vmadm {0},{1},{2}")]
-    Vmadm(Reg, Reg, Reg),
-    #[display("vmadn {0},{1},{2}")]
-    Vmadn(Reg, Reg, Reg),
-    #[display("vmadh {0},{1},{2}")]
-    Vmadh(Reg, Reg, Reg),
+    #[display("vmudl v{0},v{1},v{2}")]
+    Vmudl(usize, usize, usize),
+    #[display("vmadm v{0},v{1},v{2}")]
+    Vmadm(usize, usize, usize),
+    #[display("vmadn v{0},v{1},v{2}")]
+    Vmadn(usize, usize, usize),
+    #[display("vmadh v{0},v{1},v{2}")]
+    Vmadh(usize, usize, usize),
 }
 
 pub fn execute(vu: &mut Vu, instruction: Instruction) {
     match instruction {
-        Instruction::Vmudl(vd, vs, vt) => vu.vmudl(vd, vs, vt),
-        Instruction::Vmadm(vd, vs, vt) => vu.vmadm(vd, vs, vt),
-        Instruction::Vmadn(vd, vs, vt) => vu.vmadn(vd, vs, vt),
-        Instruction::Vmadh(vd, vs, vt) => vu.vmadh(vd, vs, vt),
+        Instruction::Vmudl(vd, vs, vt) => vu.vmudl(Reg(vd & 0x1f, None), Reg(vs & 0x1f, None), Reg(vt & 0x1f, None)),
+        Instruction::Vmadm(vd, vs, vt) => vu.vmadm(Reg(vd & 0x1f, None), Reg(vs & 0x1f, None), Reg(vt & 0x1f, None)),
+        Instruction::Vmadn(vd, vs, vt) => vu.vmadn(Reg(vd & 0x1f, None), Reg(vs & 0x1f, None), Reg(vt & 0x1f, None)),
+        Instruction::Vmadh(vd, vs, vt) => vu.vmadh(Reg(vd & 0x1f, None), Reg(vs & 0x1f, None), Reg(vt & 0x1f, None)),
     }
 }
 
