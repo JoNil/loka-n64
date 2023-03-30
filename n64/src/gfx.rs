ef2971d1cd015ded7eabdd7cdf7433a00511d20c
Jonathan Nilsson <jonathan@voysys.se>
2023-03-30 22:16:10 +0200

    Command buffer n64

--- n64/src/gfx.rs
+++ n64/src/gfx.rs
@@ -2,11 +2,14 @@ pub use command_buffer::{CommandBuffer, CommandBufferCache};
 pub use pipeline::{CycleType, FillPipeline, Pipeline, ZMode, ZSrc};
 pub use texture::{StaticTexture, Texture, TextureAlignment, TextureMut};
 
-#[cfg(target_vendor = "nintendo64")]
-mod command_buffer;
+mod command_buffer_n64;
 
 #[cfg(not(target_vendor = "nintendo64"))]
 mod command_buffer_emu;
+
+#[cfg(target_vendor = "nintendo64")]
+use command_buffer_n64 as command_buffer;
+
 #[cfg(not(target_vendor = "nintendo64"))]
 use command_buffer_emu as command_buffer;
 
